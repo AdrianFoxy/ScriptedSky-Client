898d3ac3f48721494afffd4365974fb7f5614297
AdrianFoxy <drugakadrian@gmail.com>
2024-09-23 06:53:13 +0300

    angular met styling, new section for home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
         'secondary': '#01001a',
         'primary': '#0160ff',
         'white': '#ffffff',
-        'title': '#1b3764'
+        'title': '#1b3764',
+        'card-body': '#f4f8ff',
+        'primary-hover': '#6c9df3'
       }
     },
   },
